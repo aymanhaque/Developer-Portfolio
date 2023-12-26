6855e4074bd743b2fcc789f849466f6def6d9103
aymanhaque <muhd.ayman.haque@gmail.com>
2023-12-26 14:38:59 -0600

    Just some additions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Nav from "./components/Nav";
-
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div className="bg-slate-900">
+    <>
+    <div className=" bg-slate-900">
       <Nav/>
     </div>
-  )
+    <div>
+      <Home/>
+    </div>
+    </>
+  );
 };
 
 export default App;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,27 +3,13 @@ import { GrProjects } from "react-icons/gr";
 import { IoMdContact } from "react-icons/io";
 
 const Nav = () => {
-  // const content = <>
-  <div className="">
-    <ul>
-      <button>
-        <li>Home</li>
-      </button>
-      <button>
-        <li>Projects</li>
-      </button>
-      <button>
-        <li>Contact</li>
-      </button>
-    </ul>
-  </div>;
   return (
     <nav>
       <div className="h-10vh flex justify-center text-white px-20 py-4">
         <div className=" flex items-center justify-center font-normal">
             <ul className="flex gap-8 mr-16 text-{18px}">
               <button>
-                <li><GoHomeFill/></li>
+                <li ><GoHomeFill/></li>
               </button>
               <button>
                 <li><GrProjects/></li>
