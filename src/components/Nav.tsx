8f3f25ca16da0a9b2b070c21d3a30255a1959bdb
aymanhaque <muhd.ayman.haque@gmail.com>
2023-12-30 00:19:38 -0600

    f

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,24 +4,27 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faListCheck } from "@fortawesome/free-solid-svg-icons";
 const Nav = () => {
   return (
-    <nav className="sticky top-0">
-      <div className="h-10vh flex justify-center text-white px-20 py-4 ">
+    <nav className="">
+      <div className="h-10vh flex justify-center text-white px-20 py-2 ">
         <div className=" flex items-center justify-center font-normal">
           <ul className="flex gap-8 mr-16 text-{18px}">
             <button >
               <li className="dark:text-white">
                 <FontAwesomeIcon icon={faHouse} size="xl" className="hover:text-4xl" />
+                <p className="hover:visible">House</p>
               </li>
             </button>
             <button>
               <li className="mx-4">
                 <FontAwesomeIcon icon={faListCheck} size="xl" className="hover:text-4xl" />
+                <p className=" hover:visible">Projects</p>
               </li>
             </button>
             <button>
               <li>
                 <FontAwesomeIcon icon={faUser} size="xl" className="hover:text-4xl"/>
               </li>
+              <p className=" hover:visible">Contact</p>
             </button>
           </ul>
         </div>
